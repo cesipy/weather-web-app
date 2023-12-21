d481577aeecfaab69821af8f2f4674be55d025c6
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-21 11:47:51 +0100

    add controller to search for location

---
+++ src/main/java/at/qe/skeleton/internal/ui/controllers/LocationController.java
@@ -0,0 +1,124 @@
+package at.qe.skeleton.internal.ui.controllers;
+
+import at.qe.skeleton.external.model.location.LocationDTO;
+import at.qe.skeleton.external.services.LocationApiRequestService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import org.apache.commons.text.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+import java.util.List;
+
+@Controller // @Controller is a specification of @Component
+public class LocationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
+
+    private final int LIMIT = 1;        // we want to get only one result for a location
+
+    private String locationName;
+    private String currentLocation;
+
+    private LocationDTO currentLocationDTO = null;
+
+    @Autowired
+    private LocationApiRequestService locationApiRequestService;
+
+    public void search() {
+        try {
+            searchLocation(locationName);
+
+            LocationDTO foundLocation = currentLocationDTO;
+
+            FacesMessage message;
+            if (foundLocation != null) {
+                message = new FacesMessage("Location found!", "Name: " + foundLocation.name() +
+                        ", Lat&Lon: " + foundLocation.latitude() + ", " + foundLocation.longitude());
+            } else {
+                message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Location not found!", null);
+            }
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        } catch (Exception e) {
+            LOGGER.error("Error in location search", e);
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error in location search", null);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    private void searchLocation(String locationName) {
+        try {
+            List<LocationDTO> answer = this.locationApiRequestService.retrieveLocations(locationName, LIMIT);
+
+            // Check if the list is not empty
+            if (!answer.isEmpty()) {
+                // only process the first entry in the List of LocationDTOs
+                LocationDTO firstLocation = answer.get(0);
+
+                LOGGER.info("location name: " + firstLocation.name());
+                LOGGER.info("location lat&lon: " + firstLocation.latitude() + ", " + firstLocation.longitude());
+
+                ObjectMapper mapper = new ObjectMapper()
+                        .findAndRegisterModules()
+                        .enable(SerializationFeature.INDENT_OUTPUT);
+
+                String plainTextAnswer = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(firstLocation);
+                String escapedHtmlAnswer = StringEscapeUtils.escapeHtml4(plainTextAnswer);
+                String escapedHtmlAnswerWithLineBreaks = escapedHtmlAnswer.replace("\n", "<br>")
+                        .replace(" ", "&nbsp;");
+                this.setCurrentLocation(escapedHtmlAnswerWithLineBreaks);
+                this.setCurrentLocationDTO(firstLocation);
+
+                LOGGER.info("current location: " + currentLocation);
+
+            } else {
+                LOGGER.warn("The list of locations is empty.");
+            }
+        } catch (JsonProcessingException e) {
+            LOGGER.error("Error in request in locationApi", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public int getLIMIT() {
+        return LIMIT;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public LocationDTO getCurrentLocationDTO() {
+        return currentLocationDTO;
+    }
+
+    public void setCurrentLocationDTO(LocationDTO locationDTO) {
+        this.currentLocationDTO = locationDTO;
+    }
+
+    public double getLatitude() {
+        return currentLocationDTO.latitude();
+    }
+
+    public double getLongitude() {
+        return currentLocationDTO.longitude();
+    }
+}
