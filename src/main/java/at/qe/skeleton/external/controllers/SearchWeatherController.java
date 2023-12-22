ec6caf5d1e00f99fb0d9394ea71f25e47d181a18
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-22 11:51:19 +0100

    controller for searching weather based on city name

---
+++ src/main/java/at/qe/skeleton/external/controllers/SearchWeatherController.java
@@ -0,0 +1,86 @@
+package at.qe.skeleton.external.controllers;
+
+import at.qe.skeleton.external.model.currentandforecast.CurrentAndForecastAnswerDTO;
+import at.qe.skeleton.external.model.location.LocationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SearchWeatherController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SearchWeatherController.class);
+
+    @Autowired
+    private WeatherController weatherController;
+
+    @Autowired
+    private LocationController locationController;
+
+    private CurrentAndForecastAnswerDTO currentAndForecastAnswerDTO;
+
+    private String currentWeather;
+
+    private LocationDTO currentlLocationDTO;
+
+    private String currentLocation;
+
+    private String locationToSearch;
+
+    public void searchWeatherByLocation() {
+        locationController.setLocationName(locationToSearch);
+
+        locationController.search();
+        setCurrentLocationDTO(locationController.getCurrentLocationDTO());
+        setCurrentLocation(locationController.getCurrentLocation());
+
+        weatherController.setLatitude(currentlLocationDTO.latitude());
+        weatherController.setLongitude(currentlLocationDTO.longitude());
+        weatherController.requestWeather();
+        setCurrentAndForecastAnswerDTO(weatherController.getCurrentWeatherDTO());
+        setCurrentWeather(weatherController.getCurrentWeather());
+
+        LOGGER.info("weather string in searchWeatherController: " + currentWeather);
+    }
+
+    public CurrentAndForecastAnswerDTO getCurrentAndForecastAnswerDTO() {
+        return currentAndForecastAnswerDTO;
+    }
+
+    public void setCurrentAndForecastAnswerDTO(CurrentAndForecastAnswerDTO currentAndForecastAnswerDTO) {
+        this.currentAndForecastAnswerDTO = currentAndForecastAnswerDTO;
+    }
+
+    public String getCurrentWeather() {
+        return currentWeather;
+    }
+
+    public void setCurrentWeather(String currentWeatherString) {
+        this.currentWeather = currentWeatherString;
+    }
+
+    public LocationDTO getCurrentlLocationDTO() {
+        return currentlLocationDTO;
+    }
+
+    public void setCurrentLocationDTO(LocationDTO currentlLocationDTO) {
+        this.currentlLocationDTO = currentlLocationDTO;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public String getLocationToSearch() {
+        return locationToSearch;
+    }
+
+    public void setLocationToSearch(String locationToSearch) {
+        this.locationToSearch = locationToSearch;
+    }
+}
