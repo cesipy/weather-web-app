f5afb85f619a9af565c470e196fc07ab1a16f3ad
Harrasser Florian <florian.harrasser@student.uibk.ac.at>
2024-01-13 18:25:16 +0100

    Notification System implemented

---
+++ src/main/java/at/qe/skeleton/internal/ui/controllers/EmailController.java
@@ -0,0 +1,24 @@
+package at.qe.skeleton.internal.ui.controllers;
+
+import at.qe.skeleton.internal.services.EmailService;
+import at.qe.skeleton.internal.ui.beans.EmailRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send-email")
+    @ResponseBody
+    public String sendEmail(@RequestBody EmailRequest emailRequest) {
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+        return "Email sent successfully!";
+    }
+}
+
