beffe6299680492735598efb7a7e19fd2ef11e3f
Cedric Sillaber <cedric.sillaber@gmail.com>
2024-01-25 21:31:18 +0100

    improve landing page

---
+++ src/main/java/at/qe/skeleton/external/controllers/LandingPageController.java
@@ -0,0 +1,74 @@
+package at.qe.skeleton.external.controllers;
+
+import at.qe.skeleton.external.model.Favorite;
+import at.qe.skeleton.external.model.location.Location;
+import at.qe.skeleton.external.model.weather.CurrentWeatherData;
+import at.qe.skeleton.external.services.ApiQueryException;
+import at.qe.skeleton.external.services.LocationService;
+import at.qe.skeleton.external.services.WeatherService;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@Scope("view")
+public class LandingPageController {
+    private final static Logger logger = LoggerFactory.getLogger(LandingPageController.class);
+
+    @Autowired
+    private WeatherService weatherService;
+    @Autowired
+    private LocationService locationService;
+    @Autowired
+    private MessageService messageService;
+
+    private List<Favorite> defaultFavorites;
+    private List<CurrentWeatherData> landingPageWeather;
+
+    @PostConstruct
+    public void init() {
+        defaultFavorites = new ArrayList<>();
+        landingPageWeather = new ArrayList<>();
+        constructDefaultFavorites();
+        getWeatherForLandingPage();
+
+    }
+
+
+    private void getWeatherForLandingPage() {
+        try {
+
+            for (Favorite favorite : defaultFavorites) {
+                CurrentWeatherData currentWeatherData = weatherService.fetchCurrentWeather(favorite.getLocation());
+                landingPageWeather.add(currentWeatherData);
+            }
+        }
+        catch (ApiQueryException e) {
+            messageService.showWarnMessage("An error occurred.");
+        }
+    }
+
+    private void constructDefaultFavorites() {
+        try {
+            Favorite innsbruckFavorite = new Favorite();
+            Location innsbruckLocation = locationService.retrieveLocation("Innsbruck");
+            innsbruckFavorite.setLocation(innsbruckLocation);
+
+            defaultFavorites.add(innsbruckFavorite);
+        }
+        catch (Exception e) {
+            logger.error("An error occurred while default locations were processed.");
+        }
+    }
+
+    public List<CurrentWeatherData> getLandingPageWeather() {
+        return landingPageWeather;
+    }
+
+}
